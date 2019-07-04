191506ab5ecd779a9bf2ae38f46d7cea438cabdd
sangsin1993 <hardoner@naver.com>
2019-07-04 11:06:32 +0900

    java

---
+++ day01-Hello/src/hello/Hello.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		//  자바 기본 주석 //로 시작한다.
+		// 프로그램 실행에 영향을 미치지 않음.
+
+		 System.out.println("Hellow, world");
+	}
+
+}
