81b0f37616be13c7cde6295e5d4613e6938e63d0
sangsin1993 <hardoner@naver.com>
2019-07-04 15:49:49 +0900

    boolean 타입 사용
    boolean 타입 테스트
    변수 선언, 초기화, 사용

--- day01-Hello/src/hello/Hello.java
+++ day01-Hello/src/hello/Hello.java
@@ -5,7 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		//  자바 기본 주석 //로 시작한다.
 		// 프로그램 실행에 영향을 미치지 않음.
-
+		
+		//자바는 대소문자가 틀리면 구문오류인 경우
+		//많으므로 대소문자 오타주의 하세요.
 		 System.out.println("Hellow, world");
 	}
 
@@ -14,9 +16,10 @@ public class Hello {
 		//실행 : ctrl + F11
 		//자바 기본 출력 구문
 		// sysout -> ctrl + space bar
-		sysout 
-		
+		// System.out.println();
+		 
+		 
 		// 소스 편집 후 항상 저장하는 습관
-		
+ 		
 		
 		
