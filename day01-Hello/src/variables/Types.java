6e0189c51f18d83f06071a09a53aeaec491b8a24
sangsin1993 <hardoner@naver.com>
2019-07-05 10:04:03 +0900

    gitignore

--- day01-Hello/src/variables/Types.java
+++ day01-Hello/src/variables/Types.java
@@ -39,19 +39,3 @@ public class Types {
 }
 
 
-		// 1. 선언
-		boolean isLikeBaseball;
-		boolean isExists;
-		boolean isHave;
-		boolean isEnd = false; // 선언과 동시에 초기화
-		
-		// 2. 초기화
-		isLikebaseball = true;
-		isExists = true;
-		isHave = false;
-		
-		// 3. 사용
-		System.out.println("isLikeBaseball=" + isLikeBaseball);
-		System.out.println("isExists=" + isExists);
-		System.out.println("isHave=" + isHave);
-		System.out.println("isEnd=" + isEnd);
---
+++ day02-types/src/type/primitive/BooleanTest.java
@@ -0,0 +1,25 @@
+package type.primitive;
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. 선언
+		boolean isLikeBaseball;
+		boolean isExists;
+		boolean isHave;
+		boolean isEnd = false; // 선언과 동시에 초기화
+		
+		// 2. 초기화
+		isLikeBaseball = true;
+		isExists = true;
+		isHave = false;
+		
+		// 3. 사용
+		System.out.println("isLikeBaseball=" + isLikeBaseball);
+		System.out.println("isExists=" + isExists);
+		System.out.println("isHave=" + isHave);
+		System.out.println("isEnd=" + isEnd);
+	}
+
+}
